76ec38a66813532f8372bdf1ed626eebe19df6ca
Kailash <kailash.nagarajan@vodafone.com>
2022-07-06 23:49:46 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ameya is my life! :)
         </p>
         <a
           className="App-link"
